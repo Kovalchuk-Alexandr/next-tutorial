0c2357c179a49074bfa540daec578646fdf03140
Kovalchuk-Alexandr <mo250568kaa@gmail.com>
2025-10-28 11:27:34 +0200

    aliases & fonts

---
+++ src/app/about/page.jsx
@@ -0,0 +1,14 @@
+const AboutPage = () => {
+	return (
+		<section className="content">
+			<h2 className="section-title">About</h2>
+			<p>
+				Next.js - это мощный фреймворк поверх React, используемый
+				ведущими компаниями для разработки производительных и
+				масштабируемых приложений.
+			</p>
+		</section>
+	);
+};
+
+export default AboutPage;
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,30 +1,34 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Footer from "@/components/Footer";
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Footer from '@components/Footer';
+import Navigation from '@components/Navigation';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const inter = Inter({
+	variable: '--font-inter',
+	subsets: ['cyrillic', 'latin'],
+	display: 'swap',
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+// 	variable: '--font-geist-mono',
+// 	subsets: ['latin'],
+// });
 
 export const metadata = {
-  title: "Next tutorial",
-  description: "Next tutorial app",
+	title: 'Next tutorial',
+	description: 'Next tutorial app',
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-		<div className="bg-grid"></div>
-		<main className="container page">{children}</main>
-		<Footer />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className={`${inter.variable}`}>
+			<body>
+				<div className="bg-grid"></div>
+				<Navigation />
+
+				<main className="container page">{children}</main>
+				<Footer />
+			</body>
+		</html>
+	);
 }
